92c3696e808d00e13078d260003722289aeeb42f
Algorhythmics <intivioli@gmail.com>
2024-10-07 18:40:41 +0200

    Update.

---
+++ src/main/java/gg/dystellar/dystellar_backend/api/RestDownloads.java
@@ -0,0 +1,15 @@
+package gg.dystellar.dystellar_backend.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping
+public class RestDownloads {
+   
+   @PostMapping("")
+   public String getDiff() {
+      
+   }
+}
